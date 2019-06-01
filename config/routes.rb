ac1df02ed3879d01f6b5ba3f527fcd9f300b0a85
Michael Brown <Vurclash@gmail.com>
2019-06-01 10:50:22 -0600

    configured routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
-  get 'locations/index'
-  get 'locations/new'
-  get 'locations/edit'
-  get 'locations/show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
->>>>>>> daaa096efd53442a08dd63d8831af6a55da840b6
+  root "trips#index"
+
+  resources :trips do
+    resources :locations
+
+  end
+
+  resources :locations do
+    resources :addresses
+  end
 end
