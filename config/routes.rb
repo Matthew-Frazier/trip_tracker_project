48d050c6beb38fe51bc57d932c37f788d454b4a2
Michael Brown <Vurclash@gmail.com>
2019-06-01 10:55:39 -0600

    conflict resolution

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
   root "trips#index"
 
   resources :trips do
@@ -13,5 +9,4 @@ Rails.application.routes.draw do
   resources :locations do
     resources :addresses
   end
->>>>>>> ac1df02ed3879d01f6b5ba3f527fcd9f300b0a85
 end
