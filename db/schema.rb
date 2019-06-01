a24abc1d347ec50000dc46ae35a1f8eb2a827777
pompucha <benavidesga@gmail.com>
2019-06-01 10:39:09 -0600

    address model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_163608) do
+ActiveRecord::Schema.define(version: 2019_06_01_163827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "name"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zip"
+    t.bigint "location_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["location_id"], name: "index_addresses_on_location_id"
+  end
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -35,5 +47,6 @@ ActiveRecord::Schema.define(version: 2019_06_01_163608) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "addresses", "locations"
   add_foreign_key "locations", "trips", column: "trips_id"
 end
